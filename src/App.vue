ae30f875c69c70f83deac8aea77aeec0c96c3f00
Damien Heulin <damienheulin87@gmail.com>
2021-08-25 13:07:54 +0200

    fix: Removed header border bottom @min-width: 768px

--- src/App.vue
+++ src/App.vue
@@ -461,7 +461,6 @@ footer {
   \*------------------------------------*/
   header {
     padding: 0.5rem 1rem;
-    border-bottom: 1px solid var(--clr-lighter);
   }
 
   /*------------------------------------*\
